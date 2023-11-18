7a05a605ce6e4c1bbc487b3a7a6cf086a28b2170
Mohammad Yousuf <mohammadbil1998@gmail.com>
2023-11-17 22:00:06 -0600

    Implemented singleGameRound Function

--- game.js
+++ game.js
@@ -13,12 +13,36 @@ function getComputerChoice() {
 
 // Plays a single round of the game
 function singleGameRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    
     if(playerSelection == computerSelection) {
         return 'Its a Tie!';
     }
-    else if(playerSelection == 'Rock' && computerSelection == 'Paper') {
-        return 'You Win!, Rock beats Paper!';
+    else if(playerSelection == 'rock' && computerSelection == 'paper') {
+        console.log(`Computer Picked: ${computerSelection}`)
+        return 'You Lose!, Paper beats Rock!';
+    }
+    else if(playerSelection == 'rock' && computerSelection == 'scissor') {
+        console.log(`Computer Picked: ${computerSelection}`)
+        return 'You Win!, Rock beats Scissor!';
+    }
+    else if(playerSelection == 'paper' && computerSelection == 'rock') {
+        console.log(`Computer Picked: ${computerSelection}`)
+        return 'You Win!, Paper beats Rock!';
+    }
+    else if(playerSelection == 'paper' && computerSelection == 'scissor') {
+        console.log(`Computer Picked: ${computerSelection}`)
+        return 'You Lose!, Scissor beats Paper!';
+    }
+    else if(playerSelection == 'scissor' && computerSelection == 'rock') {
+        console.log(`Computer Picked: ${computerSelection}`)
+        return 'You Lose!, Rock beats Scissor!';
+    }
+    else if(playerSelection == 'scissor' && computerSelection == 'paper') {
+        console.log(`Computer Picked: ${computerSelection}`)
+        return 'You Win!, Scissor beats Paper!';
     }
 }
 
-// console.log(singleGameRound('Rock', getComputerChoice()));
+console.log(singleGameRound('Rock', getComputerChoice()));
