400455598e4ed260e79c1071194ff728d8beffc5
Mohammad Bilal <42048110+mbilal1109@users.noreply.github.com>
2023-11-29 20:15:12 -0600

    Reworking more logic for UI

--- game.js
+++ game.js
@@ -1,68 +1,63 @@
 let playerCount = 0;
 let computerCount = 0;
+const buttons = document.querySelectorAll("button");
 
 function getComputerChoice() {
   randomChoice = Math.floor(Math.random() * 3);
   if (randomChoice == 0) {
-    return "Rock";
+    return "rock";
   } else if (randomChoice == 1) {
-    return "Paper";
+    return "paper";
   } else {
-    return "Scissor";
+    return "scissor";
   }
 }
 
 // Plays a single round of the game
 function playRound(playerSelection, computerSelection) {
-  playerSelection = playerSelection.toLowerCase();
-  computerSelection = computerSelection.toLowerCase();
-  const resultBox = document.querySelector("#results");
-  resultBox.textContent = `AI Selected: ${computerSelection}`;
+  let tempPhrase = '';
 
   if (playerSelection == computerSelection) {
-    console.log("Its a Tie!");
+    tempPhrase = "Its a Tie!";
   } else if (playerSelection == "rock") {
     if (computerSelection == "paper") {
-      console.log("You Lose!, Paper beats Rock!");
-      computerCount += 1;
+      tempPhrase = "You Lose!, Paper beats Rock!";
+      computerCount++;
     } else if (computerSelection == "scissor") {
-      console.log("You Win!, Rock beats Scissor!");
-      playerCount += 1;
+      tempPhrase = "You Win!, Rock beats Scissor!";
+      playerCount++;
     }
   } else if (playerSelection == "paper") {
     if (computerSelection == "rock") {
-      console.log("You Win!, Paper beats Rock!");
-      playerCount += 1;
+      tempPhrase = "You Win!, Paper beats Rock!";
+      playerCount++;
     } else if (computerSelection == "scissor") {
-      console.log("You Lose!, Scissor beats Paper!");
-      computerCount += 1;
+      tempPhrase = "You Lose!, Scissor beats Paper!";
+      computerCount++;
     }
   } else {
     if (computerSelection == "rock") {
-      console.log("You Lose!, Rock beats Scissor!");
-      computerCount += 1;
+      tempPhrase = "You Lose!, Rock beats Scissor!";
+      computerCount++;
     } else if (computerSelection == "paper") {
-      console.log("You Win!, Scissor beats Paper!");
-      playerCount += 1;
+      tempPhrase = "You Win!, Scissor beats Paper!";
+      playerCount++;
     }
   }
-  console.log(playerCount)
-  console.log(computerCount)
+
   if (playerCount == 5) {
-    console.log("You Win!!!");
-  } else if(computerCount == 5) {
-    console.log("AI Wins!!!");
+    tempPhrase += "<p>You Win!!!</p>";
+  } else if (computerCount == 5) {
+    tempPhrase += "<p>Computer Wins!!!</p>";
   }
+
+  document.getElementById('results').innerHTML = `<p> ${tempPhrase} </p>`;
+  document.getElementById('scores').innerHTML = `Player: ${playerCount} <br> Computer: ${computerCount}`;
   return;
 }
 
-function game() {
-  const buttons = document.querySelectorAll("button");
-  buttons.forEach((button) => {
-    button.addEventListener("click", function () {
-      playRound(button.value, getComputerChoice());
-    });
+buttons.forEach((button) => {
+  button.addEventListener("click", function () {
+    playRound(button.value, getComputerChoice());
   });
-}
-
-game();
+});
